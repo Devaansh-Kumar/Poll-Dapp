33d4ac3c2b86f4581a10f87679100e3c9eea8d49
Devaansh Kumar <devaansh840@LAPTOP-1GJ1OTPU.localdomain>
2023-01-04 15:03:12 +0530

    updated deploy script and created file for verifing the contract

--- deploy/deploy-Poll.js
+++ deploy/deploy-Poll.js
@@ -1,4 +1,6 @@
-  const { network } = require("hardhat");
+const { network } = require("hardhat");
+const developmentChains = ["hardhat", "localhost"];
+const { verify } = require("../utils/verify");
 
 module.exports = async ({getNamedAccounts, deployments}) => {
     const { deploy, log, get } = deployments;
@@ -11,6 +13,13 @@ module.exports = async ({getNamedAccounts, deployments}) => {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1,
     });
+
+    if (
+      !developmentChains.includes(network.name) &&
+      process.env.ETHERSCAN_API_KEY
+    ) {
+      await verify(pollContract.address, []);
+    }
 };
 
 module.exports.tags = ["all", "poll"];
